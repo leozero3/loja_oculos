301449bf44435b1d2b2f03b0772923591d4bc84b
leozero3 <realleo2009@hotmail.com>
2020-08-03 21:04:41 -0300

    estrutura

--- lib/view/layout.dart
+++ lib/view/layout.dart
@@ -1,15 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Layout {
   static Widget render(
-      BuildContext context,
-      Widget child, {
-        String title,
-        Widget floatingActionButton,
-        int bottomItemSelected,
-      })
-  {
+    BuildContext context,
+    Widget child, {
+    Widget floatingActionButton,
+    int bottomItemSelected,
+  }) {
     return Scaffold(
       body: SafeArea(
         child: Stack(
@@ -22,8 +21,40 @@ class Layout {
                 ),
               ),
             ),
-            Center(
-              child: Text('data'),
+            Column(
+              children: [
+                Container(
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                        child: FaIcon(
+                          FontAwesomeIcons.userCog,
+                          color: Layout.Light(),
+                          size: 24,
+                        ),
+                      ),
+                      Expanded(
+                          child: Text(
+                        'Leonardo Gonzalez',
+                        style: Theme.of(context).textTheme.subtitle2.copyWith(
+                            color: Layout.Light(),
+                            fontSize: 18,
+                            fontStyle: FontStyle.italic),
+                      )),
+                      Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: FaIcon(
+                          FontAwesomeIcons.shoppingBag,
+                          color: Layout.primaryLight(),
+                          size: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(child: child)
+              ],
             ),
             Expanded(child: child),
           ],
@@ -32,43 +63,74 @@ class Layout {
       floatingActionButton: floatingActionButton,
       backgroundColor: Layout.secundary(),
       bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.wb_sunny, size: 30,),
-              title: Text('Inicio'),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.wb_sunny,
+              size: 30,
             ),
-            BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.solidStar, size: 30,),
-              title: Text('Compras'),
-            ),BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.solidHeart, size: 30,),
-              title: Text('Favoritos'),
-            ),BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.signOutAlt, size: 30,),
-              title: Text('Sair'),
+            title: Text('Inicio'),
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.solidStar,
+              size: 30,
             ),
-          ],
+            title: Text('Compras'),
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.solidHeart,
+              size: 30,
+            ),
+            title: Text('Favoritos'),
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.signOutAlt,
+              size: 30,
+            ),
+            title: Text('Sair'),
+          ),
+        ],
         currentIndex: bottomItemSelected ?? 0,
-        selectedItemColor: (bottomItemSelected == null ) ? Layout.Dark(.3) : Layout.primaryLight(),
+        selectedItemColor: (bottomItemSelected == null)
+            ? Layout.Dark(.3)
+            : Layout.primaryLight(),
         unselectedItemColor: Layout.Dark(.3),
         backgroundColor: Layout.Light(),
         type: BottomNavigationBarType.fixed,
-        onTap: (int i){
-            print('item $i');
+        onTap: (int i) {
+          print('item $i');
         },
       ),
     );
   }
 
-  static Color primary([double opacity = 1]) => Color(0xff195738).withOpacity(opacity);
-  static Color primaryLight([double opacity = 1]) => Color(0xff007d40).withOpacity(opacity);
-  static Color primaryDark([double opacity = 1]) => Color(0xff123D27).withOpacity(opacity);
+  static Color primary([double opacity = 1]) =>
+      Color(0xff195738).withOpacity(opacity);
 
-  static Color secundary([double opacity = 1]) => Color(0xffddc199).withOpacity(opacity);
-  static Color secundaryLight([double opacity = 1]) => Color(0xffE0CF9D).withOpacity(opacity);
-  static Color secundaryDark([double opacity = 1]) => Color( 0xffce9150).withOpacity(opacity);
-  static Color secundaryHight([double opacity = 1]) => Color(0xffFDAC25).withOpacity(opacity);
+  static Color primaryLight([double opacity = 1]) =>
+      Color(0xff007d40).withOpacity(opacity);
 
-  static Color Light([double opacity = 1]) => Color(0xfff0ece1).withOpacity(opacity);
-  static Color Dark([double opacity = 1]) => Color(0xff333333).withOpacity(opacity);
+  static Color primaryDark([double opacity = 1]) =>
+      Color(0xff123D27).withOpacity(opacity);
+
+  static Color secundary([double opacity = 1]) =>
+      Color(0xffddc199).withOpacity(opacity);
+
+  static Color secundaryLight([double opacity = 1]) =>
+      Color(0xffE0CF9D).withOpacity(opacity);
+
+  static Color secundaryDark([double opacity = 1]) =>
+      Color(0xffce9150).withOpacity(opacity);
+
+  static Color secundaryHight([double opacity = 1]) =>
+      Color(0xffFDAC25).withOpacity(opacity);
+
+  static Color Light([double opacity = 1]) =>
+      Color(0xfff0ece1).withOpacity(opacity);
+
+  static Color Dark([double opacity = 1]) =>
+      Color(0xff333333).withOpacity(opacity);
 }
