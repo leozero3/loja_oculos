1cd029f26e6f271e400f7e4573663327921b12c4
leozero3 <realleo2009@hotmail.com>
2020-08-06 17:51:45 -0300

    roda 3

--- lib/view/home/roda_categoria.dart
+++ lib/view/home/roda_categoria.dart
@@ -11,6 +11,12 @@ class _RodaCategoriaState extends State<RodaCategoria>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
 
+  //para tween 1 = 360º
+  //para transform.rotate = pi * 2 = 360º
+
+  double _startDeg = 0.0;
+  double _endDeg = 0.0;
+
   @override
   void initState() {
     super.initState();
@@ -39,10 +45,14 @@ class _RodaCategoriaState extends State<RodaCategoria>
 
         ///ANIMAÇÃO RODA -------------------------------------------------------
         RotationTransition(
-          turns: Tween(begin: 0.0, end: -1.0).animate(_controller),
+          turns: Tween(begin: _startDeg, end: _endDeg).animate(_controller),
           child: GestureDetector(
             onTap: () {
-              _controller.forward().then((e) => _controller.reset());
+              _controller.reset();
+              _startDeg = _endDeg;
+              _endDeg += 0.25;
+
+              _controller.forward();
             },
             child: Container(
               width: MediaQuery.of(context).size.width,
@@ -50,78 +60,108 @@ class _RodaCategoriaState extends State<RodaCategoria>
               margin: EdgeInsets.only(top: 10, bottom: 10),
               decoration: BoxDecoration(shape: BoxShape.circle),
               child: Stack(
-                children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(
-                        MediaQuery.of(context).size.width),
-                    child: Image.asset(
-                      'assets/images/bg-catwheel.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20),
-                          child: Icon(
-                            Icons.translate,
-                            color: Colors.white,
-                            size: 32,
-                          ),
-                        ),
-                        Text(
-                          'Estilo',
-                          style: Theme.of(context)
-                              .textTheme
-                              .subtitle2
-                              .copyWith(color: Layout.Light()),
-                        )
-                      ],
-                    ),
-                  ),
-                  Transform.rotate(
-                    angle: pi / 2,
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.red,
-                      ),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Icon(
-                              Icons.add_shopping_cart,
-                              color: Colors.white,
-                              size: 32,
-                            ),
-                          ),
-                          Text(
-                            'compras',
-                            style: Theme.of(context)
-                                .textTheme
-                                .subtitle2
-                                .copyWith(color: Layout.Light()),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+                children: _getCategorias(),
               ),
             ),
           ),
         ),
       ],
     );
+  }
+
+  List<Widget> _getCategorias() {
+    List<Widget> result = [];
+
+    result.add(
+      ClipRRect(
+        borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width),
+        child: Image.asset(
+          'assets/images/bg-catwheel.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+
+    for (Map<String, dynamic> item in _getListaCategorias()) {
+      result.add(
+        Transform.rotate(
+          angle: pi * 2,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Icon(
+                    item['icon'],
+                    color: Colors.white,
+                    size: 32,
+                  ),
+                ),
+                Text(
+                  item['text'],
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle2
+                      .copyWith(color: Layout.Light()),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return result;
+  }
+
+  /// Model de itens para serem impressos na roda
+  List<Map<String, dynamic>> _getListaCategorias() {
+    return [
+      {
+        "id": 1,
+        "icon": Icons.favorite,
+        "text": 'Estilo',
+      },
+      {
+        "id": 2,
+        "icon": Icons.filter_drama,
+        "text": 'Teen',
+      },
+      {
+        "id": 3,
+        "icon": Icons.flight,
+        "text": 'Viagem',
+      },
+      {
+        "id": 4,
+        "icon": Icons.store_mall_directory,
+        "text": 'Trabalho',
+      },
+      {
+        "id": 5,
+        "icon": Icons.style,
+        "text": 'Casual',
+      },
+      {
+        "id": 6,
+        "icon": Icons.supervised_user_circle,
+        "text": 'Executivo',
+      },
+      {
+        "id": 7,
+        "icon": Icons.switch_video,
+        "text": 'Esporte',
+      },
+      {
+        "id": 8,
+        "icon": Icons.thumb_up,
+        "text": 'Classico',
+      },
+    ];
   }
 }
